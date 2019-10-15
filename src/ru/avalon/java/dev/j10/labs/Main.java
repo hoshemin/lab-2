79ef25b7de826655b0cc8a7d0ce34d8ae19c76e0
Daniel Alpatov <danial.alpatov@gmail.com>
2019-10-16 01:19:42 +0300

    Initial commit

---
+++ src/ru/avalon/java/dev/j10/labs/Main.java
@@ -0,0 +1,23 @@
+package ru.avalon.java.dev.j10.labs;
+
+import ru.avalon.java.dev.j10.labs.shapes.Shape;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Shape[] shapes = null;
+
+        /*
+         * TODO: Выполнить действия над массивом 'shapes'
+         *
+         * 1. Проинициализировать переменную 'shapes' массивом
+         *    содержащим 10 произвольных фигур. Массив должен
+         *    содержать экземпляры классов Circle, Rectangle
+         *    и Triangle.
+         *
+         * 2. Найти в массиве 'shapes' фигуру с максимальной
+         *    площадью. Для поиска фигуры необходимо создать
+         *    статический метод в текущем классе (Main).
+         */
+    }
+}
