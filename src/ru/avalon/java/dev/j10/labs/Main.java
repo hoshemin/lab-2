ef942c50f81b81c428398f551503bb365c91c4bb
hoshemin <56467614+hoshemin@users.noreply.github.com>
2019-10-25 15:57:32 +0300

    Update Main.java

--- src/ru/avalon/java/dev/j10/labs/Main.java
+++ src/ru/avalon/java/dev/j10/labs/Main.java
@@ -1,12 +1,42 @@
 package ru.avalon.java.dev.j10.labs;
 
-import ru.avalon.java.dev.j10.labs.shapes.Shape;
+import ru.avalon.java.dev.j10.labs.shapes.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Shape[] shapes = null;
+        Shape[] shapes = new Shape[10];
 
+        for(int i=0; i<10; i++){
+        int n = (int) (Math.random()*3);
+         
+        switch(n){
+                case 0:
+                shapes[i] = new Circle();
+                break;
+                case 1:
+                shapes[i] = new Rectangle();
+                break;                
+                default:
+                shapes[i] = new Triangle();
+        }
+        }   
+        
+        Shape max = null;
+        
+        for(int j = 0; j < shapes.length; j++){
+            System.out.println(shapes[j] + "|Area " + shapes[j].getArea() + " m2|"); 
+            if(max != null){          
+                if(shapes[j].getArea() > max.getArea()){
+                    max = shapes[j];
+                }
+            }
+                else {
+                    max = shapes[j];
+                }
+        }
+            
+        System.out.println("\n" + "Max area for figure " + max + "is " + max.getArea() + " m2"); 
         /*
          * TODO: Выполнить действия над массивом 'shapes'
          *
